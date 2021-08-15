ed6e89815e1a7ed81596351d544a9933506011ff
Anthony Chu <anthony@anthonychu.ca>
2021-08-14 22:47:50 -0700

    Add debugging

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,14 @@
 // vue.config.js
 module.exports = {
-    chainWebpack: config => {
-      config
-        .plugin('html')
-        .tap(args => {
-          args[0].title = 'Static Web Apps Chat'
-          return args
-        })
-    }
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Static Web Apps Chat'
+        return args
+      })
+  },
+  configureWebpack: {
+    devtool: 'source-map'
   }
+}
